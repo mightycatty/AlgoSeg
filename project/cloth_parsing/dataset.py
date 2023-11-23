98eaf9fc037772925dc0ad7a9a2524e38bfafdb6
shuai.he <shuai.he@ur.com.cn>
2023-11-23 09:20:47 +0800

    sync

--- project/cloth_parsing/dataset.py
+++ project/cloth_parsing/dataset.py
@@ -2,12 +2,12 @@
 dataset_type = 'BaseSegDataset'
 data_root = '/home/justsomeone/data/seg/dresscode_bytedance_parsing'
 crop_size = (512, 512)
-
+IGNORE_ZERO_LABEL = False
 metainfo = dict(
-    classes=('1', '2', '3', '4', '5', '6', '7',
+    classes=('0','1', '2', '3', '4', '5', '6', '7',
              '8', '9', '10', '11', '12', '13', '14',
              '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25'),
-    palette=[[120, 120, 120], [180, 120, 120], [6, 230, 230], [80, 50, 50],
+    palette=[[0, 0, 0],[120, 120, 120], [180, 120, 120], [6, 230, 230], [80, 50, 50],
                  [4, 200, 3], [120, 120, 80], [140, 140, 140], [204, 5, 255],
                  [230, 230, 230], [4, 250, 7], [224, 5, 255], [235, 255, 7],
                  [150, 5, 61], [120, 120, 70], [8, 255, 51], [255, 6, 82],
@@ -17,7 +17,7 @@ metainfo = dict(
 
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', reduce_zero_label=True),
+    dict(type='LoadAnnotations', reduce_zero_label=IGNORE_ZERO_LABEL),
     dict(
         type='RandomResize',
         scale=(2048, 512),
@@ -33,7 +33,7 @@ test_pipeline = [
     dict(type='Resize', scale=(2048, 512), keep_ratio=True),
     # add loading annotation after ``Resize`` because ground truth
     # does not need to do resize data transform
-    dict(type='LoadAnnotations', reduce_zero_label=True),
+    dict(type='LoadAnnotations', reduce_zero_label=IGNORE_ZERO_LABEL),
     dict(type='PackSegInputs')
 ]
 img_ratios = [0.5, 0.75, 1.0, 1.25, 1.5, 1.75]
@@ -62,7 +62,7 @@ train_dataloader = dict(
         data_root=data_root,
         img_suffix='.png',
         metainfo=metainfo,
-        reduce_zero_label=True,
+        reduce_zero_label=IGNORE_ZERO_LABEL,
         data_prefix=dict(
             img_path='train/image', seg_map_path='train/mask'),
         pipeline=train_pipeline))
@@ -76,7 +76,7 @@ val_dataloader = dict(
         img_suffix='.png',
         metainfo=metainfo,
         data_root=data_root,
-        reduce_zero_label=True,
+        reduce_zero_label=IGNORE_ZERO_LABEL,
         data_prefix=dict(
             img_path='val/image', seg_map_path='val/mask'),
     pipeline=test_pipeline))
--- readme.md
+++ readme.md
@@ -1,10 +1,18 @@
-# custom codebase for segmentation
-
-
-## Training 
+# Personal repo for segmentation
+SETUP
+```bash
+# install mmseg
+cd mmsegmentation
+pip install -e .
+# install segment_anything
+```
+---
+## WIKI
+### Custom dataset training with mmseg
 1. prepare img/ and mask/ folders
 ```text
 Ref:https://mmsegmentation.readthedocs.io/en/main/advanced_guides/add_datasets.html#customize-datasets-by-reorganizing-data
+mask: pixel value in range[0, class_num index-1] # setup metainfo in dataset.py
 ```
 2. prepare data_config.py and config.py
 ```text
@@ -14,3 +22,9 @@ Ref:https://mmsegmentation.readthedocs.io/en/main/advanced_guides/add_datasets.h
 ```bash
 python ../../mmsegmentation/tools/train.py upernet_r50.py --amp --work-dir ./test
 ```
+4. inference
+```bash
+python project/cloth_parsing/image_demo_scrips.py
+```
+### Custom module for mmseg
+https://mmsegmentation.readthedocs.io/en/latest/advanced_guides/add_models.html
